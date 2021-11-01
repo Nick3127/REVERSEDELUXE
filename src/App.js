8365c11679e2ca227d1d1223efdf996df66924f0
Nicko <90507034+Nick3127@users.noreply.github.com>
2021-11-02 00:49:36 +0500

    test Parallax for Reverse

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  const [offsetY, setOffsetY] = useState(0);
+
+  const handleScrollTop = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollTop);
+
+    return () => window.removeEventListener("scroll", handleScrollTop);
+  }, []);
+
+  const renderContent = () => (
+    <>
+      <div className="Parallax__content__heading">
+        <a href='https://www.instagram.com/reversls/?utm_medium=copy_link' className="Parallax__content__heading__text">REVERSE</a>
+        <h2 className="Parallax__content__heading__caption">
+          Nitinur in vetitum semper, cupimusque negate...
+          {/* — Мы всегда стремимся к запретному и желаем недозволенного (Овидий, «Любовные элегии») */}
+        </h2>
+      </div>
+      <div className="Parallax__content__cta">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h2>Check my profile!</h2>
+         <h2> <a href='https://www.instagram.com/vrfew/?hl=ru'>RTWOER</a> </h2>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
+  );
+
+  return (
+    <selection className="Parallax">
+      <div
+        className="Parallax__background"
+        style={{ transform: `translateY(${offsetY * 0.7}px` }}
+      />
+      <div
+        className="Parallax__background-triangles"
+        style={{ transform: `translateY(${offsetY * 0.3}px` }}
+      />
+      <div
+        className="Parallax__background-triangles2"
+        style={{ transform: `translateY(${offsetY * 0.5}px` }}
+      />
+      <div className="Parallax__content">{renderContent()}</div>
+    </selection>
   );
 }
 
